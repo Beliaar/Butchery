bcfcdf4a94b5dbbc51a78db0196c273a64c38be8
Beliar <KarstenBock@gmx.net>
2012-10-06 13:31:09 +0200

    Removed falsely committed test code.

--- src/butchery/common/Limestone.java
+++ src/butchery/common/Limestone.java
@@ -5,7 +5,7 @@ import net.minecraft.src.CreativeTabs;
 import net.minecraft.src.Item;
 import net.minecraft.src.ItemStack;
 
-public class Limestone extends Item implements ITubWaterModifier {
+public class Limestone extends Item {
 
 	public Limestone(int item_id) {
 		super(item_id);
@@ -18,33 +18,5 @@ public class Limestone extends Item implements ITubWaterModifier {
 		return CommonProxy.ITEMS_PNG;
 	}
 
-	@Override
-	public ItemStack getOutput(ItemStack input) {
-		if (input.getItem().getClass() == DeadCow.class) {
-			return new ItemStack(Item.leather, 1);
-		}
-		return null;
-	}
-
-	@Override
-	public int getMaxAmountTransformed(ItemStack input) {
-		if (input.getItem().getClass() == DeadCow.class) {
-			return Math.min(2, input.stackSize);
-		}
-		return 0;
-	}
-
-	@Override
-	public int getTicksNeeded(ItemStack input) {
-		return 100;
-	}
-
-	@Override
-	public int getWaterUsage(ItemStack input) {
-		if (input.getItem().getClass() == DeadCow.class) {
-			return 10;
-		}
-		return 0;
-	}
 
 }
