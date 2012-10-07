4b0cec5ee2ccb6ae7a1606c7862107c246438f11
Beliar <KarstenBock@gmx.net>
2012-10-07 14:54:35 +0200

    Added copyright notice to files that didn't have it.

--- src/butchery/common/gui/SlotTubOutput.java
+++ src/butchery/common/gui/SlotTubOutput.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Beliar, 2012
+ * https://github.com/Beliaar/Butchery
+ *
+ * Butchery is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license located in
+ * https://github.com/Beliaar/Butchery/wiki/License
+ */
 package butchery.common.gui;
 
 import net.minecraft.src.IInventory;
